f7abb2cb889b7f50113f8e156e43a2cca4f76257
mattbr0wn <matt@openline.ai>
2024-06-01 17:47:49 +0100

    add deploy script

---
+++ deploy/deploy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+HOME = os.path.expanduser("~")
+
+
+def run_command(command):
+    subprocess.run(command, shell=True, check=True)
+
+
+def rebuild():
+    run_command("git pull")
+    build = HOME + "cmd/website.go build"
+    run_command(f"go run {build}")
+
+
+def restart_caddy():
+    docker_compose = HOME + "/caddy/docker_compose.yaml"
+    run_command(f"sudo docker compose -f {docker_compose} restart")
+
+
+if __name__ == "__main__":
+    rebuild()
+    restart_caddy()
